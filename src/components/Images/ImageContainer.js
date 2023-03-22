ba29d0648d3f1358f9ffb438b4b0b83a983255d1
Sanket Koli <kolisanket15@gmail.com>
2023-03-21 21:47:28 -0700

    test

--- src/components/Images/ImageContainer.js
+++ src/components/Images/ImageContainer.js
@@ -12,7 +12,7 @@ const ImageContainer = (props) => {
   // }, [hover]);
   const gridCellInvisibile = () => {
     setHover(true);
-    // console.log()
+    console.log();
   };
 
   const gridCellVisibile = () => {
