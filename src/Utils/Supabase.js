d726a2bc571cf9dc4c9033e39e931f73f9043059
Sanket Koli <kolisanket15@gmail.com>
2023-04-09 23:42:29 -0700

    supabase config variables

--- src/Utils/Supabase.js
+++ src/Utils/Supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_KEY
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY
+
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
